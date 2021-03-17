f5b1deb958743b9e3f8443a659e34b4b60584a09
Denis Astapenko <byslamdv@gmail.com>
2021-03-17 22:23:19 +0300

    first test commit

---
+++ src/com/skillfactory/module4/bookstore/Book.java
@@ -0,0 +1,69 @@
+package com.skillfactory.module4.bookstore;
+
+public class Book {
+
+    private static int count = 0;
+    private String bookName;
+    private String BookAuthor;
+    private int bookPrice;
+    private int editionYear;
+    private int bookID;
+
+    public int getBookID() {
+
+        return bookID;
+    }
+
+    public String getBookAuthor() {
+        return BookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        BookAuthor = bookAuthor;
+    }
+
+    public Book(String bookName, String bookAuthor, int editionYear, int bookPrice) {
+        this.bookName = bookName;
+        this.BookAuthor = bookAuthor;
+        this.bookPrice = bookPrice;
+        this.editionYear = editionYear;
+        this.bookID =++count;
+    }
+
+    public Book(String bookName, String BookAuthor, int editionYear) {
+        this.bookName = bookName;
+        this.BookAuthor = BookAuthor;
+        this.editionYear = editionYear;
+        this.bookID =++count;
+    }
+
+    public Book() {
+        this ("some book", "some Author", 2000);
+        bookID++;
+    }
+
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public int getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(int bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+
+    public int getEditionYear() {
+        return editionYear;
+    }
+
+    public void setEditionYear(int editionYear) {
+        this.editionYear = editionYear;
+    }
+}
---
+++ src/com/skillfactory/module4/bookstore/BookStore.java
@@ -0,0 +1,105 @@
+package com.skillfactory.module4.bookstore;
+
+public class BookStore {
+    public static void main(String[] args) {
+
+        Book[] bookList = new Book[99];
+
+        printAllBookList(bookList);
+
+        Book book0 = new Book();
+
+        addBook(bookList, book0);
+
+        Book book1 = new Book("Hobbit", "Tolkin", 1937, 99);
+
+        addBook(bookList, book1);
+
+        Book book2 = new Book("Hobbit2", "Tolkin", 1937);
+
+        Book book3 = new Book("Hobbit3", "Tolkin", 1937);
+
+        Book book4 = new Book("Kolobok", "Narod", 1701);
+
+        addBook(bookList, book2);
+        addBook(bookList, book3);
+        addBook(bookList, book4);
+
+        printAllBookList(bookList);
+
+        findAndPrintBook(bookList, "Hobbit");
+    }
+
+    public static boolean checkBook(Book[] bookList, Book book) {
+
+        for (Book item : bookList) {
+
+
+            if (item != null && item.getBookName().equals(book.getBookName()) && item.getEditionYear() == book.getEditionYear()) {
+
+                System.out.println("Данная книга уже есть в картотеке");
+                return true;
+            }
+        }
+
+        System.out.println("Данной книги нет в картотеке");
+        return false;
+    }
+
+    public static void addBook(Book[] bookList, Book book) {
+
+        if (!checkBook(bookList, book)) {
+
+            for (int i = 0; i < bookList.length; i++) {
+                if (bookList[i] == null) {
+                    bookList[i] = book;
+                    System.out.println("Книга добавлена в картотеку");
+                    break;
+                }
+                if (i == bookList.length - 1) {
+                    System.out.println("Картотека переволнена");
+                }
+            }
+
+        }
+
+    }
+
+    public static void printAllBookList(Book[] bookList) {
+
+        for (int i = 0; i < bookList.length; i++) {
+            if (bookList[i] == null && i == 0) {
+
+                System.out.println("Booklist is empty");
+            } else if (bookList[i] == null) {
+
+                System.out.println("Booklist is ending");
+                break;
+
+            } else if (bookList[i].getBookName() == "some book") {
+
+                System.out.printf("книга №%d: название: \"%s\" \n",
+                        i + 1, bookList[i].getBookName());
+            } else {
+
+                System.out.printf("книга №%d: название: \"%s\", год издания: %dг., цена - %dusd \n",
+                        i + 1, bookList[i].getBookName(), bookList[i].getEditionYear(), bookList[i].getBookPrice());
+
+            }
+        }
+    }
+
+    public static void findAndPrintBook(Book[] bookList, String book) {
+
+
+        int count = 0;
+        for (int i = 0; i < bookList.length; i++) {
+            if (bookList[i] != null && bookList[i].getBookName().contains(book)) {
+                count++;
+                System.out.printf("книга №%d: название: \"%s\", автор: \"%s\", год издания: %dг., ID книги -  %d\n",
+                        count, bookList[i].getBookName(), bookList[i].getBookAuthor(), bookList[i].getEditionYear(), bookList[i].getBookID());
+            }
+        }
+    }
+
+}
